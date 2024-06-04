0ed1bd2c8021ef9693d95a1464d27e86f7ea6c7e
Rhamseys Garcia <rhamseysgarcia@MacBook-Pro.lan>
2024-06-04 04:56:37 -0700

    Link to Update

--- client/src/components/Links/Links.jsx
+++ client/src/components/Links/Links.jsx
@@ -8,22 +8,22 @@ const Links = () => {
   return (
     <>
       <span className='links-container'>
-        <Link to='/' className='link'>
+        <Link to='https://www.Facebook.com' className='link' target="_blank" rel="noopener noreferrer">
           <FaFacebookSquare className='icon' />
         </Link>
-        <Link to='/' className='link'>
+        <Link to='https://www.Twitter.com' className='link' target="_blank" rel="noopener noreferrer">
           <FaSquareXTwitter className='icon' />
         </Link>
-        <Link to='/' className='link'>
+        <Link to='https://www.Instagram.com' className='link' target="_blank" rel="noopener noreferrer">
           <FaInstagramSquare className='icon' />
         </Link>
-        <Link to='/' className='link'>
+        <Link to='https://www.Youtube.com' className='link' target="_blank" rel="noopener noreferrer">
           <FaYoutubeSquare className='icon' />
         </Link>
-        <Link to='/' className='link'>
+        <Link to='https://www.music.apple.com' className='link' target="_blank" rel="noopener noreferrer">
           <SiApplemusic className='icon' />
         </Link>
-        <Link to='/' className='link'>
+        <Link to='https://www.Spotify.com' className='link' target="_blank" rel="noopener noreferrer">
           <FaSpotify className='icon' />
         </Link>
       </span>
